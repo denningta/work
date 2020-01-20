e7c5226db87a89842157e7862b44c370ce58425e
TD <denningta@gmail.com>
2020-01-19 17:03:50 -0700

    redirect to dashboard and dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: 'home#index'
+
+  get 'dashboard', to: 'dashboard#index'
 
 
 end
